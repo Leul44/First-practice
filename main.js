f27ae377de38dbde77e4a9345e2979a8b4d422cc
leul44 <leul441@hotmail.com>
2019-02-18 14:18:40 -0500

    updated html,css,js

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+
+/*
+let firstname = "EDEN"
+let Lastname ="Leul"
+let age = "21"
+let Gender ="F"
+console.log( firstname);
+//alert (firstname+" "+ Lastname + " " + age+" "+Gender )
+//msWriteProfilerMark( firstname)
+document. write (firstname+" "+ Lastname + " " + age+" "+Gender);
